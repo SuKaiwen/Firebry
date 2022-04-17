5418c5873ca62b7b6f6861f05cf6ffb08fd3736a
Kevin Su <kevinsu@outlook.co.nz>
2022-04-17 13:34:55 +1000

    init

---
+++ src/Firebase/config.js
@@ -0,0 +1,32 @@
+
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDHtZtLEA7RE-vXFX_ummeMXD5AtZMjUmU",
+  authDomain: "forum-75156.firebaseapp.com",
+  projectId: "forum-75156",
+  storageBucket: "forum-75156.appspot.com",
+  messagingSenderId: "693892525051",
+  appId: "1:693892525051:web:85ea9289d77d4042e7395e",
+  measurementId: "G-H15ZCR8E4F"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Storage
+const storage = firebase.storage;
+const firestore = firebase.firestore;
+
+export {storage, firestore};
